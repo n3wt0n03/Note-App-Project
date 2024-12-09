f182171d26aacbb122068db4d5e183f91710ff1d
Marc Nelson B. Ochavo <89735506+n3wt0n03@users.noreply.github.com>
2024-12-10 00:25:48 +0800

    [FEAT] Added signup component

---
+++ client/note-app/src/app/components/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // For [(ngModel)]
+import { CommonModule } from '@angular/common'; // For *ngIf and other directives
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+}
